54b837d8795e6ec8da153fed43c21c8f65af0b5e
wanghaiyang_lab306 <oceanking111@163.com>
2014-10-11 11:36:27 +0800

    rename getCurrent to upload_rs_data

--- Ckan_API_Code/filestore/upload_rs_data.sh
+++ Ckan_API_Code/filestore/upload_rs_data.sh
@@ -2,14 +2,14 @@
 #============ get the file name ===========  
 
 #循环读取文件夹名  
-pkg_id="201211-sec-fy2d-ir1"
+pkg_id="201205-tbb-fy2e"
 echo ${pkg_id}
 
 for file_a in ./*; do  
     temp_file=`basename $file_a` 
     temp_file_name=${temp_file%.*} 
     #echo ${InputDir}/${temp_file}
-    curl -H'Authorization: c4ec2786-f178-468b-8122-8c24f4175c4d' 'http://10.50.6.151/api/3/action/resource_create' \
+    curl -H'Authorization: c4ec2786-f178-468b-8122-8c24f4175c4d' 'http://202.121.178.242/api/3/action/resource_create' \
 	--form upload=@${temp_file} \
 	--form package_id=${pkg_id} \
 	--form name=${temp_file_name} \
